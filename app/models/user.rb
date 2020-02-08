a96385d478f700d89346eefb29eeddce089246f6
taisei-my-friend <idents.taisei@gmail.com>
2020-02-09 02:30:07 +0900

    Finish toy app2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates FILL_IN, presence: true    # 「FILL_IN」をコードに置き換えてください
-  validates FILL_IN, presence: true    # 「FILL_IN」をコードに置き換えてください
 end
